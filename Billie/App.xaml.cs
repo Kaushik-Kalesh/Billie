2e200254d641f2af4f1c6279a930e6734d8af0e9
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2024-07-10 21:45:59 +0530

    Updated Project Files

--- Billie/App.xaml.cs
+++ Billie/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Billie
         }
 
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
             m_window.Activate();
