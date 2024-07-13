73f7f5d489f5849e077f0fc75c8e1b8152920d43
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2024-07-13 17:38:29 +0530

    A service to work with the OFF API

---
+++ Billie/ApiService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Billie
+{
+    internal class ApiService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public class ProductResponse
+        {
+            public Product product { get; set; }
+            public int status { get; set; }
+        }
+
+        public class Product
+        {
+            public string product_name { get; set; }
+            public string brands { get; set; }
+        }
+
+        public static async Task<Product> FetchProductDetailsFromApi(long barcode)
+        {
+            var apiUrl = $"https://world.openfoodfacts.org/api/v/product/{barcode}.json";
+            var response = await client.GetAsync(apiUrl);
+
+            response.EnsureSuccessStatusCode();
+
+            var responseData = await response.Content.ReadAsStringAsync();
+            var productResponse = JsonSerializer.Deserialize<ProductResponse>(responseData);
+
+            if (productResponse.status == 0) { return null; }
+
+            return productResponse.product;
+        }
+    }
+}
