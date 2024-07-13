9fb761f6271ace1c07029fc33c3573ef0b293c13
Kaushik-Kalesh <kaushikkalesh@gmail.com>
2024-07-13 17:39:52 +0530

    Linked API to fetch product details

--- Billie/StockingPage.xaml.cs
+++ Billie/StockingPage.xaml.cs
@@ -1,4 +1,6 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System;
 using System.Collections.ObjectModel;
 
 namespace Billie
@@ -7,39 +9,40 @@ namespace Billie
     {
         public int ID { get; set; }
         public string ProductName { get; set; }
-        public string Category { get; set; }
+        public string Brand { get; set; }
         public float Price { get; set; }
-        public int Barcode { get; set; }
+        public long Barcode { get; set; }
         public int StockQuantity { get; set; }
-
-        public Product(int id, string productName, string category, float price, int barcode, int stockQuantity)
-        {
-            ID = id;
-            ProductName = productName;
-            Category = category;
-            Price = price;
-            Barcode = barcode;
-            StockQuantity = stockQuantity;
-        }
     }
 
     public sealed partial class StockingPage : Page
     {
-        public ObservableCollection<Product> Products { get; set; }
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product>();
 
         public StockingPage()
         {
-            this.InitializeComponent();
-            Products = new ObservableCollection<Product>
-            {
-                new Product(1, "Lays", "Food", 25, 567890, 50)
-            };
+            this.InitializeComponent();            
             CustomerDataGrid.ItemsSource = Products;
-        }
+        }        
 
-        private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-        {
-            Products.Add(new Product(0, null, null, 0, 0, 0));
+        private async void AddProductButton_Click(object sender, RoutedEventArgs e)
+        {            
+            if (NewBarcodeTextBox.Text.Length == 13 && Int64.TryParse(NewBarcodeTextBox.Text, out long barcode) == true)
+            {                
+                var product = await ApiService.FetchProductDetailsFromApi(barcode);
+                if (product != null)
+                {
+                    Products.Add(new Product
+                    {
+                        ID = Products.Count + 1,
+                        ProductName = product.product_name,
+                        Brand = product.brands,
+                        Price = 0,
+                        Barcode = barcode,
+                        StockQuantity = 0
+                    });
+                }
+            }             
         }
     }
 }
